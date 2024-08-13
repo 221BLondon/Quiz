b73bae4c1d459c2dc519422459f112baedf437e8
221BLondon <sahadsalimh@gmail.com>
2024-08-13 13:15:26 +0530

    Update question_loader.py

--- question_loader.py
+++ question_loader.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from pathlib import Path
 
+# Load questions from CSV file
 def load_questions(file_path):
-    data_filename = Path(file_path)
-    return pd.read_csv(data_filename)
+    DATA_FILENAME = Path(__file__).parent / 'new.csv'
+    return pd.read_csv(DATA_FILENAME)
